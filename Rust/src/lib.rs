e85e9c0d8176a06b9480318fc16b12ca7b0e468b
Brandon Roehl <me@brandonroehl.org>
2026-01-31 12:01:24 -0600

    cell

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -5,22 +5,34 @@
 use std::os::raw::c_int;
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+impl Field {
+    fn cell<'a>(self: Field, x: c_int, y: c_int) -> Option<&'a mut bool> {
+        // New index
+        let i: c_int = x + y * self.w;
+        // Keep this from getting out of bounds
+        if i > 0 && self.w * self.h < i {
+            unsafe {
+                let ptr: *mut bool = self.s.add(i as usize);
+
+                ptr.as_mut()
+            }
+        } else {
+            None
+        }
+    }
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn Set(self_: &mut Field, x: c_int, y: c_int, b: bool) {
-    // Keep this from getting out of bounds
-    assert!(self_.w * self_.h < x * y);
-    // New index
-    let i: usize = (x + y * self_.w) as usize;
-
-    unsafe {
-       self_.s.add(i).write(b);
-    };
+    *self_.cell(x, y).expect("out of range") = b;
 }
 
-// unsafe extern "C" {
-//     pub fn Get(self_: *mut Field, x: ::std::os::raw::c_int, y: ::std::os::raw::c_int) -> bool;
-// }
+#[unsafe(no_mangle)]
+pub extern "C" fn Get(self_: &mut Field, x: c_int, y: c_int) -> bool {
+    *self_.cell(x, y).expect("out of range")
+}
+
+
 // unsafe extern "C" {
 //     pub fn Next(self_: *mut Field, x: ::std::os::raw::c_int, y: ::std::os::raw::c_int) -> bool;
 // }
