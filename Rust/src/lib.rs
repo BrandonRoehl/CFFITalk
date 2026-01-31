9e6f323a0789518e47331be825389516ccf80a38
Brandon Roehl <me@brandonroehl.org>
2026-01-31 12:36:28 -0600

    next

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -7,12 +7,12 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 // MARK: - Get and Set will be implemented together
 impl Field {
-    fn cell<'a>(self: Field, x: c_int, y: c_int) -> &'a mut bool {
+    fn cell<'a>(self: &'a mut Field, x: c_int, y: c_int) -> &'a mut bool {
+        let x = x % self.w;
+        let y = y % self.h;
         // New index
         let i: c_int = x + y * self.w;
         // Keep this from getting out of bounds
-        assert!(i >= 0, "index negative");
-        assert!(self.w * self.h < i, "index out of range");
         unsafe {
             let ptr: *mut bool = self.s.add(i as usize);
 
@@ -31,13 +31,25 @@ pub extern "C" fn Get(self_: &mut Field, x: c_int, y: c_int) -> bool {
     *self_.cell(x, y)
 }
 
-//MARK: - Next state of a specific cell
+// MARK: - Next state of a specific cell
+pub extern "C" fn Next(self_: &mut Field, x: c_int, y: c_int) -> bool {
+	// Count the adjacent cells that are alive.
+	let mut alive = 0;
+	for i in -1..=1 {
+	    for j in -1..=1 {
+			if (j != 0 || i != 0) && *self_.cell(x+i, y+j) {
+				alive += 1;
+			}
+		}
+	}
+	// Return next state according to the game rules:
+	//   exactly 3 neighbors: on,
+	//   exactly 2 neighbors: maintain current state,
+	//   otherwise: off.
+	alive == 3 || alive == 2 && *self_.cell(x, y)
+}
 
-// unsafe extern "C" {
-//     pub fn Next(self_: *mut Field, x: ::std::os::raw::c_int, y: ::std::os::raw::c_int) -> bool;
-// }
-
-//MARK: - Increment the game
+// MARK: - Increment the game
 
 // unsafe extern "C" {
 //     pub fn Step(self_: *mut Life, x: ::std::os::raw::c_int, y: ::std::os::raw::c_int);
