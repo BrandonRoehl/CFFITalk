87fbc2e24a8db1815ecc10b0e3405c1d982856dd
Brandon Roehl <me@brandonroehl.org>
2026-01-31 20:54:31 -0600

    last

--- Rust/src/lib.rs
+++ Rust/src/lib.rs
@@ -5,6 +5,7 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 use std::os::raw::c_int;
+use std::ptr;
 
 // MARK: - Get and Set will be implemented together
 impl Field {
@@ -58,5 +59,18 @@ pub unsafe extern "C" fn Next(self_: &mut Field, x: c_int, y: c_int) -> bool {
 
 #[unsafe(no_mangle)]
 pub unsafe extern "C" fn Step(self_: &mut Life) {
+    unsafe {
+        let a = &mut *self_.a;
+        let b = &mut *self_.b;
+        // Update the state of the next field (b) from the current field (a).
+        for y in 0..self_.h {
+            for x in 0..self_.w {
+                Set(b, x, y, Next(a, x, y));
+            }
+        }
+    }
+
+	// Swap fields a and b.
+    unsafe { ptr::swap(self_.a, self_.b) };
 }
 
