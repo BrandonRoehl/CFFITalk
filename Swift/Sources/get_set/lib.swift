45f175063037e18d7cadd978a7faea1d76b20b6a
Brandon Roehl <me@brandonroehl.org>
2026-02-03 19:31:29 -0600

    wow

--- Swift/Sources/get_set/lib.swift
+++ Swift/Sources/get_set/lib.swift
@@ -1,6 +1,23 @@
 import CLink
 
-public func Get(self: UnsafePointer<Field>, x: Int, y: Int) -> Bool {
-    // return CLink_get(self, x)
-    return false
+private func cell(_ f: UnsafeMutablePointer<Field>, x: Int32, y: Int32) -> UnsafeMutablePointer<
+    Bool
+> {
+    // If the x or y coordinates are outside the field boundaries they are wrapped
+    // toroidally. For instance, an x value of -1 is treated as width-1.
+    let x = Int(x % f.pointee.w)
+    let y = Int(y % f.pointee.h)
+
+    let xptr = f.pointee.s.advanced(by: x).pointee!
+    let xyptr = xptr.advanced(by: y)
+
+    return xyptr
+}
+
+public func Get(self f: UnsafeMutablePointer<Field>, x: Int32, y: Int32) -> Bool {
+    return cell(f, x: x, y: y).pointee
+}
+
+public func Set(self f: UnsafeMutablePointer<Field>, x: Int32, y: Int32, b: Bool) {
+    return cell(f, x: x, y: y).initialize(to: b)
 }
