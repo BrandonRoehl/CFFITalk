aa38cb47f6c5e2056c36e4b2e070aba7a3791bf5
Brandon Roehl <me@brandonroehl.org>
2026-02-08 08:12:35 -0600

    go main

---
+++ Go/cli/main.go
@@ -0,0 +1,42 @@
+//go:build cgo
+
+package main
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/../../include/
+#cgo LDFLAGS: -l c_get_set -l c_next -l c_step
+#include "../../link.h"
+*/
+import "C"
+
+import "unsafe"
+
+func newField(w, h int) Field {
+	size := unsafe.Sizeof(C.bool(false))
+	f := Field{
+		xyfield: make([]C.bool, w*h),
+		xfield:  make([]*C.bool, w),
+
+		Field: C.Field{
+			w: C.int(w),
+			h: C.int(h),
+		},
+	}
+	xyfield := unsafe.Pointer(&f.xyfield)
+	for x := range w {
+		f.xfield[x] = (*C.bool)(unsafe.Add(xyfield, uintptr(x*h)*size))
+	}
+	return f
+}
+
+type Field struct {
+	xyfield []C.bool
+	xfield  []*C.bool
+	C.Field
+}
+
+func main() {
+	f := newField(10, 10)
+
+	_ = &f.Field
+}
